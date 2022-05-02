523933a433ef433a7de8936de9598487ee73f906
Saaappi <alex.metz1024@gmail.com>
2022-05-01 19:33:03 -0700

    Orphan and micro zones should now use a proper parent.

--- ExpBuddy.lua
+++ ExpBuddy.lua
@@ -1,8 +1,48 @@
 local addonName, addonTable = ...
 local e = CreateFrame("Frame")
 
+local function UpdateZone()
+	local currentMapId = C_Map.GetBestMapForUnit("player")
+	local currentMapInfo = C_Map.GetMapInfo(currentMapId)
+	
+	-- If the mapType isn't a zone, then let's check
+	-- the parent.
+	--
+	-- 5: Micro (e.g. caves)
+	-- 6: Orphan (e.g. Thunder Totem)
+	if currentMapInfo.mapType == 5 or currentMapInfo.mapType == 6 then
+		addonTable.currentMap = C_Map.GetMapInfo(currentMapInfo.parentMapID).name
+	else
+		addonTable.currentMap = currentMapInfo.name
+	end
+	
+	-- If the player's current zone isn't in the
+	-- table, then add it.
+	--
+	-- If the player's current zone isn't in the
+	-- table, then none of the subtables will be
+	-- either.
+	if ExpBuddyDB[addonTable.currentMap] == nil then
+		ExpBuddyDB[addonTable.currentMap] = {}
+		ExpBuddyDB[addonTable.currentMap]["Quests"] = 0
+		ExpBuddyDB[addonTable.currentMap]["Kills"] = 0
+		ExpBuddyDB[addonTable.currentMap]["Rested"] = 0
+		ExpBuddyDB[addonTable.currentMap]["Nodes"] = 0
+		ExpBuddyDB[addonTable.currentMap]["Exploration"] = 0
+	end
+	
+	if ExpBuddyPctDB[addonTable.currentMap] == nil then
+		ExpBuddyPctDB[addonTable.currentMap] = {}
+		ExpBuddyPctDB[addonTable.currentMap]["Quests"] = 0
+		ExpBuddyPctDB[addonTable.currentMap]["Kills"] = 0
+		ExpBuddyPctDB[addonTable.currentMap]["Nodes"] = 0
+		ExpBuddyPctDB[addonTable.currentMap]["Exploration"] = 0
+	end
+end
+
 e:RegisterEvent("ADDON_LOADED")
 e:RegisterEvent("PLAYER_LEVEL_UP")
+e:RegisterEvent("ZONE_CHANGED")
 e:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 e:SetScript("OnEvent", function(self, event, ...)
 	if event == "ADDON_LOADED" then
@@ -79,32 +119,10 @@ e:SetScript("OnEvent", function(self, event, ...)
 			end
 		end)
 	end
+	if event == "ZONE_CHANGED" then
+		UpdateZone()
+	end
 	if event == "ZONE_CHANGED_NEW_AREA" then
-		local currentMapId = C_Map.GetBestMapForUnit("player")
-		local currentMapInfo = C_Map.GetMapInfo(currentMapId)
-		addonTable.currentMap = currentMapInfo.name
-		
-		-- If the player's current zone isn't in the
-		-- table, then add it.
-		--
-		-- If the player's current zone isn't in the
-		-- table, then none of the subtables will be
-		-- either.
-		if ExpBuddyDB[addonTable.currentMap] == nil then
-			ExpBuddyDB[addonTable.currentMap] = {}
-			ExpBuddyDB[addonTable.currentMap]["Quests"] = 0
-			ExpBuddyDB[addonTable.currentMap]["Kills"] = 0
-			ExpBuddyDB[addonTable.currentMap]["Rested"] = 0
-			ExpBuddyDB[addonTable.currentMap]["Nodes"] = 0
-			ExpBuddyDB[addonTable.currentMap]["Exploration"] = 0
-		end
-		
-		if ExpBuddyPctDB[addonTable.currentMap] == nil then
-			ExpBuddyPctDB[addonTable.currentMap] = {}
-			ExpBuddyPctDB[addonTable.currentMap]["Quests"] = 0
-			ExpBuddyPctDB[addonTable.currentMap]["Kills"] = 0
-			ExpBuddyPctDB[addonTable.currentMap]["Nodes"] = 0
-			ExpBuddyPctDB[addonTable.currentMap]["Exploration"] = 0
-		end
+		UpdateZone()
 	end
 end)
