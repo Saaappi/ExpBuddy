98b3868fdb7bfa674e5fc86909c6986051aa4b9a
Saaappi <alex.metz1024@gmail.com>
2023-02-23 17:40:06 -0800

    More stuff!

--- Ace.lua
+++ Ace.lua
@@ -4,17 +4,6 @@ local xpcall = xpcall
 
 ExpBuddy = LibStub("AceAddon-3.0"):NewAddon("ExpBuddy", "AceConsole-3.0")
 
-local function FormatNumber(number)
-	local formattedNumber = number
-	while true do  
-		formattedNumber, k = string.gsub(formattedNumber, "^(-?%d+)(%d%d%d)", '%1,%2')
-		if (k==0) then
-			break
-		end
-	end
-	return formattedNumber
-end
-
 function ExpBuddy:SlashCommandHandler(cmd)
 	local cmd, arg1, arg2 = string.split(" ", cmd)
 	if not cmd or cmd == "" then
@@ -32,34 +21,34 @@ function ExpBuddy:SlashCommandHandler(cmd)
 		--tinsert(UISpecialFrames, "ExpBuddyTrackerFrame")
 		
 		-- Monsters Label
-		local monstersXP = FormatNumber(tostring(ExpBuddyDB[addonTable.currentMap]["Monsters"]))
-		local monstersLabel = AceGUI:Create("Label")
-		monstersLabel:SetText(CreateAtlasMarkup("ShipMission_DangerousSkull", 16, 16) .. " |cffFFD100" .. "Monsters|r: " .. monstersXP)
-		frame:AddChild(monstersLabel)
+		local monstersXP = addonTable.FormatNumber(tostring(ExpBuddyDB[addonTable.currentMap]["Monsters"]))
+		addonTable.monstersLabel = AceGUI:Create("Label")
+		addonTable.monstersLabel:SetText(CreateAtlasMarkup("ShipMission_DangerousSkull", 16, 16) .. " |cffFFD100" .. "Monsters|r: " .. monstersXP)
+		frame:AddChild(addonTable.monstersLabel)
 		
 		-- Rested XP Label
-		local restedXP = FormatNumber(tostring(ExpBuddyDB[addonTable.currentMap]["Rested"]))
-		local restedLabel = AceGUI:Create("Label")
-		restedLabel:SetText("\n |T136090:0|t" .. " |cffFFD100" .. "Rested|r: " .. restedXP)
-		frame:AddChild(restedLabel)
+		local restedXP = addonTable.FormatNumber(tostring(ExpBuddyDB[addonTable.currentMap]["Rested"]))
+		addonTable.restedLabel = AceGUI:Create("Label")
+		addonTable.restedLabel:SetText("\n |T136090:0|t" .. " |cffFFD100" .. "Rested|r: " .. restedXP)
+		frame:AddChild(addonTable.restedLabel)
 		
 		-- Quests Label
-		local questsXP = FormatNumber(tostring(ExpBuddyDB[addonTable.currentMap]["Quests"]))
-		local questsLabel = AceGUI:Create("Label")
-		questsLabel:SetText("\n" .. CreateAtlasMarkup("NPE_TurnIn", 16, 16) .. " |cffFFD100" .. "Quests|r: " .. questsXP)
-		frame:AddChild(questsLabel)
+		local questsXP = addonTable.FormatNumber(tostring(ExpBuddyDB[addonTable.currentMap]["Quests"]))
+		addonTable.questsLabel = AceGUI:Create("Label")
+		addonTable.questsLabel:SetText("\n" .. CreateAtlasMarkup("NPE_TurnIn", 16, 16) .. " |cffFFD100" .. "Quests|r: " .. questsXP)
+		frame:AddChild(addonTable.questsLabel)
 		
 		-- Nodes Label
-		local nodesXP = FormatNumber(tostring(ExpBuddyDB[addonTable.currentMap]["Nodes"]))
-		local nodesLabel = AceGUI:Create("Label")
-		nodesLabel:SetText("\n" .. CreateAtlasMarkup("Mobile-TreasureIcon", 16, 16) .. " |cffFFD100" .. "Nodes|r: " .. nodesXP)
-		frame:AddChild(nodesLabel)
+		local nodesXP = addonTable.FormatNumber(tostring(ExpBuddyDB[addonTable.currentMap]["Nodes"]))
+		addonTable.nodesLabel = AceGUI:Create("Label")
+		addonTable.nodesLabel:SetText("\n" .. CreateAtlasMarkup("Mobile-TreasureIcon", 16, 16) .. " |cffFFD100" .. "Nodes|r: " .. nodesXP)
+		frame:AddChild(addonTable.nodesLabel)
 		
 		-- Exploration Label
-		local explorationXP = FormatNumber(tostring(ExpBuddyDB[addonTable.currentMap]["Exploration"]))
-		local explorationLabel = AceGUI:Create("Label")
-		explorationLabel:SetText("\n" .. CreateAtlasMarkup("GarrMission_MissionIcon-Exploration", 16, 16) .. " |cffFFD100" .. "Exploration|r: " .. explorationXP)
-		frame:AddChild(explorationLabel)
+		local explorationXP = addonTable.FormatNumber(tostring(ExpBuddyDB[addonTable.currentMap]["Exploration"]))
+		addonTable.explorationLabel = AceGUI:Create("Label")
+		addonTable.explorationLabel:SetText("\n" .. CreateAtlasMarkup("GarrMission_MissionIcon-Exploration", 16, 16) .. " |cffFFD100" .. "Exploration|r: " .. explorationXP)
+		frame:AddChild(addonTable.explorationLabel)
 		
 		-- Entry Level Editbox
 		local entryLevelEditbox = AceGUI:Create("EditBox")
--- Data/Constants.lua
+++ Data/Constants.lua
@@ -1,4 +1,16 @@
 local addonName, addonTable = ...
+
+addonTable.FormatNumber = function(number)
+	local formattedNumber = number
+	while true do  
+		formattedNumber, k = string.gsub(formattedNumber, "^(-?%d+)(%d%d%d)", '%1,%2')
+		if (k==0) then
+			break
+		end
+	end
+	return formattedNumber
+end
+
 local data = {
 	-- Numbers
 	["MAX_LEVEL"] 			= 70,
