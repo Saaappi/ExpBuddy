9fbf0684d3fc7caaece1d3591c51e5118f24c9e3
Saaappi <alex.metz1024@gmail.com>
2024-05-10 10:49:26 -0700

    Nodes should now work.

--- Data/Nodes.lua
+++ Data/Nodes.lua
@@ -1,32 +1,26 @@
-local addonName, addonTable = ...
-local e = CreateFrame("Frame")
-local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
-local maxLevel = 60
+local addonName, addon = ...
+local eventHandler = CreateFrame("Frame")
 
-local function FindNumber(str, nth)
-	local arr = {}
-	for i in string.gmatch(str, "%d+") do
-		table.insert(arr, i)
-	end
-	return arr[nth]
-end
-
-e:RegisterEvent("CHAT_MSG_COMBAT_XP_GAIN")
-e:SetScript("OnEvent", function(self, event, ...)
+-- "Nodes" are treasure, herbs, ore, etc.
+eventHandler:RegisterEvent("CHAT_MSG_COMBAT_XP_GAIN")
+eventHandler:SetScript("OnEvent", function(self, event, ...)
 	if event == "CHAT_MSG_COMBAT_XP_GAIN" then
-		if addonTable.playerLevel == maxLevel then return end
-		
+		if addon.playerLevel == GetMaxLevelForPlayerExpansion() then
+			-- The player is max level, so unregister the event and return.
+			eventHandler:UnregisterEvent("CHAT_MSG_COMBAT_XP_GAIN")
+			return
+		end
+
 		local msg = ...
-		if string.find(msg, "experience%.") and not string.find(msg, "dies") then
+		if string.find(msg, "experience%.") and (not string.find(msg, "dies")) then
 			-- The player looted a node for some experience, so
 			-- let's add it to the Nodes experience for the current
 			-- map.
-			-- Nodes are considered treasures, herbs, ore, etc.
-			local experience = ExpBuddyDataDB[addonTable.currentMap]["Nodes"]
-			local nodeXP = tonumber(FindNumber(msg, 1))
-			experience = experience + nodeXP
-			ExpBuddyDataDB[addonTable.currentMap]["Nodes"] = experience
-			addonTable.nodesLabel:SetText("\n" .. CreateAtlasMarkup("Mobile-TreasureIcon", 16, 16) .. " |cffFFD100" .. "Nodes|r: " .. addonTable.FormatNumber(tostring(experience)) .. " (" .. addonTable.CalculatePercent(ExpBuddyDataDB[addonTable.currentMap]["Nodes"]) .. "%)")
+			local currentExperience = ExpBuddyDataDB[addon.mapID].Nodes
+			local experienceGained = tonumber(addon.FindNumber(msg, 1))
+			local newExperience = currentExperience + experienceGained
+			ExpBuddyDataDB[addon.mapID].Nodes = newExperience
+			--addonTable.nodesLabel:SetText("\n" .. CreateAtlasMarkup("Mobile-TreasureIcon", 16, 16) .. " |cffFFD100" .. "Nodes|r: " .. addonTable.FormatNumber(tostring(experience)) .. " (" .. addonTable.CalculatePercent(ExpBuddyDataDB[addonTable.currentMap]["Nodes"]) .. "%)")
 		end
 	end
 end)
