ae7ea5e218e9442ba26e8db0834ef9b74009f054
Saaappi <alex.metz1024@gmail.com>
2024-05-10 10:53:20 -0700

    The Quests code should now work.

--- Data/Quests.lua
+++ Data/Quests.lua
@@ -1,32 +1,36 @@
-local addonName, addonTable = ...
-local e = CreateFrame("Frame")
+local addonName, addon = ...
+local eventHandler = CreateFrame("Frame")
 
-e:RegisterEvent("QUEST_TURNED_IN")
-e:SetScript("OnEvent", function(self, event, ...)
+eventHandler:RegisterEvent("QUEST_TURNED_IN")
+eventHandler:SetScript("OnEvent", function(self, event, ...)
 	if event == "QUEST_TURNED_IN" then
-		if addonTable.playerLevel == addonTable.data["MAX_LEVEL"] then return end
-		
-		local _, questXP = ...
+		if addon.playerLevel == GetMaxLevelForPlayerExpansion() then
+			-- The player is max level, so unregister the event and return.
+			eventHandler:UnregisterEvent("QUEST_TURNED_IN")
+			return
+		end
+
+		local _, experienceGained = ...
 		C_Timer.After(1, function()
-			local experience = 0
-			if questXP > 0 then
+			local newExperience = 0
+			if experienceGained > 0 then
 				-- The player completed a quest for some experience, so
 				-- let's add it to the Quests experience for the current
 				-- map.
-				experience = ExpBuddyDataDB[addonTable.currentMap]["Quests"]
-				experience = experience + questXP
-				ExpBuddyDataDB[addonTable.currentMap]["Quests"] = experience
+				local currentExperience = ExpBuddyDataDB[addon.mapID].Quests
+				newExperience = currentExperience + experienceGained
+				ExpBuddyDataDB[addon.mapID].Quests = newExperience
 			end
-			
+
 			-- This event will also add to the Nodes category, but we can
 			-- safely deduct the quest experience from the Nodes category
 			-- to remove the false positive.
-			if ExpBuddyDataDB[addonTable.currentMap]["Nodes"] ~= 0 then
-				ExpBuddyDataDB[addonTable.currentMap]["Nodes"] = ExpBuddyDataDB[addonTable.currentMap]["Nodes"] - questXP
-				addonTable.nodesLabel:SetText("\n" .. CreateAtlasMarkup("Mobile-TreasureIcon", 16, 16) .. " |cffFFD100" .. "Nodes|r: " .. addonTable.FormatNumber(tostring(ExpBuddyDataDB[addonTable.currentMap]["Nodes"])) .. " (" .. addonTable.CalculatePercent(ExpBuddyDataDB[addonTable.currentMap]["Nodes"]) .. "%)")
+			if ExpBuddyDataDB[addon.mapID].Nodes ~= 0 then
+				ExpBuddyDataDB[addon.mapID].Nodes = ExpBuddyDataDB[addon.mapID].Nodes - experienceGained
+				--addonTable.nodesLabel:SetText("\n" .. CreateAtlasMarkup("Mobile-TreasureIcon", 16, 16) .. " |cffFFD100" .. "Nodes|r: " .. addonTable.FormatNumber(tostring(ExpBuddyDataDB[addonTable.currentMap]["Nodes"])) .. " (" .. addonTable.CalculatePercent(ExpBuddyDataDB[addonTable.currentMap]["Nodes"]) .. "%)")
 			end
-			
-			addonTable.questsLabel:SetText("\n" .. CreateAtlasMarkup("NPE_TurnIn", 16, 16) .. " |cffFFD100" .. "Quests|r: " .. addonTable.FormatNumber(tostring(experience)) .. " (" .. addonTable.CalculatePercent(ExpBuddyDataDB[addonTable.currentMap]["Quests"]) .. "%)")
+
+			--addonTable.questsLabel:SetText("\n" .. CreateAtlasMarkup("NPE_TurnIn", 16, 16) .. " |cffFFD100" .. "Quests|r: " .. addonTable.FormatNumber(tostring(experience)) .. " (" .. addonTable.CalculatePercent(ExpBuddyDataDB[addonTable.currentMap]["Quests"]) .. "%)")
 		end)
 	end
 end)
