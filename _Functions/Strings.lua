c0333a23a22c3a816ba1de0b1d61b8738e0bedef
Saaappi <alex.metz1024@gmail.com>
2024-05-10 11:19:36 -0700

    Added a comment.

--- _Functions/Strings.lua
+++ _Functions/Strings.lua
@@ -1,5 +1,7 @@
 local addonName, addon = ...
 
+-- This function is used when the zone name is just a little
+-- too long.
 addon.Substring = function(str)
     if str:len() <= 15 then
 		return str
