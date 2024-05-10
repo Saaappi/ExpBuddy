776a44dc221467a5ed2030ee867fdfd586f138b1
Saaappi <alex.metz1024@gmail.com>
2024-05-10 14:46:05 -0700

    Removed the 'reset' command.

--- _Commands/Commands.lua
+++ _Commands/Commands.lua
@@ -6,7 +6,5 @@ SlashCmdList["ExpBuddy"] = function(cmd)
 	local command, subcommand, arg1 = string.split(" ", cmd)
 	if not command or command == "" then
 		addon.LoadFrame()
-    elseif command == "reset" then
-        ExpBuddyPositionDB.SavedPosition = nil
 	end
 end
